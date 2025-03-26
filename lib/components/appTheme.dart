e56311c9f8f163eeaba6d5be62d372863f5679ee
Anne-15 <wariiyuanne@gmail.com>
2025-03-26 07:45:32 +0300

    setting up of the products page and app text theme.

---
+++ lib/components/appTheme.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextTheme buildTextTheme() {
+  return TextTheme(
+    displayLarge: GoogleFonts.nunitoSans(
+      fontSize: 48.0,
+      fontWeight: FontWeight.bold,
+    ),
+    displayMedium: GoogleFonts.nunitoSans(
+      fontSize: 45.0,
+      fontWeight: FontWeight.w500,
+    ),
+    displaySmall: GoogleFonts.nunitoSans(
+      fontSize: 36.0,
+      fontWeight: FontWeight.w500,
+    ),
+    headlineLarge: GoogleFonts.nunitoSans(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: GoogleFonts.nunitoSans(
+      fontSize: 28.0,
+      fontWeight: FontWeight.w500,
+    ),
+    headlineSmall: GoogleFonts.nunitoSans(
+      fontSize: 24.0,
+      fontWeight: FontWeight.w500,
+    ),
+    titleLarge: GoogleFonts.nunitoSans(
+      fontSize: 22.0,
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: GoogleFonts.nunitoSans(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w500,
+    ),
+    titleSmall: GoogleFonts.nunitoSans(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+    ),
+    labelLarge: GoogleFonts.nunitoSans(
+      fontSize: 16.0,
+      fontWeight: FontWeight.bold,
+    ),
+    labelMedium: GoogleFonts.nunitoSans(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+    ),
+    labelSmall: GoogleFonts.nunitoSans(
+      fontSize: 12.0,
+      fontWeight: FontWeight.w500,
+    ),
+    bodyLarge: GoogleFonts.nunitoSans(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w500,
+    ),
+    bodyMedium: GoogleFonts.nunitoSans(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+    ),
+    bodySmall: GoogleFonts.nunitoSans(
+      fontSize: 12.0,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+}
